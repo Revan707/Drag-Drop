a2bcb176882a7efce64a35fd652a0a8730e7d875
Revan707 <ravangh@code.edu.az>
2023-05-11 11:46:15 +0400

    Task isn't done

---
+++ Assets/main.js
@@ -0,0 +1,42 @@
+const myDivs=document.querySelectorAll(".myDiv")
+const draggables=document.querySelectorAll(".draggable")
+draggables.forEach(draggable=>{
+    draggable.addEventListener("dragstart",()=>{
+        draggable.classList.add("draging")
+    })
+
+    draggable.addEventListener("dragend",()=>{
+        draggable.classList.remove("draging")
+    })
+})
+
+myDivs.forEach(myDiv=>{
+    myDiv.addEventListener("dragover",(e)=>{
+        e.preventDefault()
+        const afterElement=getAfterElement(myDiv,e.clientY)
+        const draggable=document.querySelector(".draging")
+        if(afterElement==null){
+            myDiv.appendChild(draggable)
+        }else{
+            myDiv.insertBefore(draggable,afterElement)
+        }
+    })
+})
+
+function getAfterElement(myDiv,y) {
+    const draggableElements=[...myDiv.querySelectorAll("draggable:not(.draging)")]
+    
+ return   draggableElements.reduce((closest,child) =>{
+        const box=child.getBoundingClientRect()
+        const offset=y-box.top-box.height /2
+        if(offset<0 &&offset>closest.offset){
+            return {offset: offset,element:child}
+        }else{
+            return closest;
+        }
+    }, {offset:Number.NEGATIVE_INFINITY}).element
+}
+
+
+
+
